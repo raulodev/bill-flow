507977cf6f41527eb4b2b20165bbc85361d077d0
raulodev <raulodev@gmail.com>
2025-04-29 07:46:07 -0400

    add custom_openapi

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from app.addresses.api import router as address_router
 from app.custom_fields.api import router as custom_fields_router
@@ -15,6 +16,20 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Bill Flow API",
+        version="1.0.0",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 app.include_router(address_router, prefix="/v1", tags=["Addresses"])
 app.include_router(custom_fields_router, prefix="/v1", tags=["Custom Fields"])
