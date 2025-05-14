477320970441771d41c9b99c530d32b12189067b
raulodev <raulodev@gmail.com>
2025-05-14 13:24:22 -0400

    update test

--- test/conftest.py
+++ test/conftest.py
@@ -2,9 +2,11 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.database.session import create_db_and_tables
 
 
 @pytest.fixture()
 def client():
     """Return test client"""
+    create_db_and_tables()
     return TestClient(app)
