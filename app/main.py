4691673edf5bb0dbc558d8e3e46756e052f19f8d
raulodev <raulodev@gmail.com>
2025-05-26 14:52:17 -0400

    sort imports

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ from fastapi.staticfiles import StaticFiles
 
 from app.accounts.api import router as account_router
 from app.addresses.api import router as address_router
+from app.credit.api import router as credit_router
 from app.custom_fields.api import router as custom_fields_router
 from app.database.session import create_db_and_tables
 from app.products.api import router as product_router
-from app.credit.api import router as credit_router
 
 
 @asynccontextmanager
