757d7c6581826cc3f65e535f16f838e3619ce8e7
raulodev <raulodev@gmail.com>
2025-05-29 23:40:37 -0400

    update cteate subscription api

--- app/subscriptions/api.py
+++ app/subscriptions/api.py
@@ -4,7 +4,11 @@ from fastapi import APIRouter, Query, status
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import select
 
-from app.database.models import Subscription, SubscriptionCreate
+from app.database.models import (
+    Subscription,
+    SubscriptionCreate,
+    SubscriptionProduct,
+)
 from app.database.session import SessionDep
 from app.exceptions import BadRequestError, NotFoundError
 from app.responses import responses
@@ -13,7 +17,34 @@ router = APIRouter(prefix="/subscriptions", responses=responses)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_subscription(
-    subscription: SubscriptionCreate, session: SessionDep
-) -> Subscription:
-    return ""
+async def create_subscription(subscription: SubscriptionCreate, session: SessionDep):
+
+    product_ids = [product.product_id for product in subscription.products]
+    if len(product_ids) != len(set(product_ids)):
+        raise BadRequestError(
+            detail="A product cannot be repeated in the same subscription."
+        )
+
+    subscription_data = subscription.model_dump(exclude={"products"})
+    subscription_db = Subscription(**subscription_data)
+
+    session.add(subscription_db)
+
+    product_entries = [
+        SubscriptionProduct(
+            product_id=product.product_id,
+            quantity=product.quantity,
+        )
+        for product in subscription.products
+    ]
+
+    subscription_db.products = product_entries
+
+    try:
+        session.commit()
+        session.refresh(subscription_db)
+        return subscription_db
+    except IntegrityError as exc:
+        raise BadRequestError(
+            detail="A product cannot be repeated in the same subscription"
+        ) from exc
