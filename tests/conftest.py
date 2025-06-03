4210917256bad2f21fb8691ca322fcad3d829412
raulodev <raulodev@gmail.com>
2025-06-03 13:59:43 -0400

    add test tenant

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
-from app.database.deps import clear_db_and_tables, create_db_and_tables, engine
+from app.database.deps import clear_db_and_tables, create_db_and_tables, engine, init_db
 from app.main import app
 
 
@@ -10,6 +10,7 @@ from app.main import app
 def client():
     """Return test client"""
     create_db_and_tables()
+    init_db()
     yield TestClient(app)
     clear_db_and_tables()
 
---
+++ tests/test_tenant.py
@@ -0,0 +1,55 @@
+from fastapi.testclient import TestClient
+from app.database.models import Tenant
+
+
+def test_create_tenant_success(client: TestClient):
+
+    data = {
+        "name": "Tenant Name",
+        "api_key": "key",
+        "api_secret": "secret-12345678",
+    }
+
+    response = client.post("/v1/tenants", json=data, auth=("admin", "password"))
+
+    assert response.status_code == 201
+
+
+def test_create_tenant_external_id_duplicate(client: TestClient):
+    data = {
+        "name": "Tenant Name",
+        "api_key": "key",
+        "api_secret": "secret-12345678",
+        "external_id": "12345678",
+    }
+
+    client.post("/v1/tenants", json=data, auth=("admin", "password"))
+    response = client.post("/v1/tenants", json=data, auth=("admin", "password"))
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "External id already exists"
+
+
+def test_read_tenants(client: TestClient, db):
+
+    tenant1 = Tenant(
+        name="1", external_id=1, api_key="key", api_secret="secret-12345678"
+    )
+    tenant2 = Tenant(
+        name="2", external_id=2, api_key="key", api_secret="secret-12345678"
+    )
+    db.add(tenant1)
+    db.add(tenant2)
+    db.commit()
+
+    response = client.get("/v1/tenants")
+
+    assert response.status_code == 200
+
+    response_json = response.json()
+
+    assert len(response_json) == 2
+
+    for account in response_json:
+        assert "external_id" in account
+        assert "api_key" in account
