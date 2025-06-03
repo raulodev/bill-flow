dc6a5d280f94554d7c241c62d3e31d20ec62629d
raulodev <raulodev@gmail.com>
2025-06-03 10:49:21 -0400

    update settings

--- app/settings.py
+++ app/settings.py
@@ -2,3 +2,5 @@ from decouple import config
 
 DATABASE_URL = config("DATABASE_URL", default="sqlite:///database.db")
 CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="redis://127.0.0.1:6379")
+ADMIN_USERNAME = config("ADMIN_USERNAME", default="admin")
+ADMIN_PASSWORD = config("ADMIN_PASSWORD", default="password")
