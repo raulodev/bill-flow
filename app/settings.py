259bc632999bba50a14c88114604a3408edd8719
raulodev <raulodev@gmail.com>
2025-06-08 13:31:02 -0400

    add log

--- app/settings.py
+++ app/settings.py
@@ -5,3 +5,4 @@ CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="redis://127.0.0.1:6379"
 ADMIN_USERNAME = config("ADMIN_USERNAME", default="admin")
 ADMIN_PASSWORD = config("ADMIN_PASSWORD", default="password")
 TIME_ZONE = config("TIME_ZONE", default="UTC")
+LOG_LEVEL = config("LOG_LEVEL", default="INFO")
