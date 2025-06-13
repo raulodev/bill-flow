8d8d3c7e83b416d286ccb7d9083ed217aafa286f
raulodev <raulodev@gmail.com>
2025-06-13 11:36:41 -0400

    add log

--- app/credit/api.py
+++ app/credit/api.py
@@ -4,6 +4,7 @@ from sqlmodel import select
 from app.database.deps import CurrentTenant, SessionDep
 from app.database.models import Account, CreditBase, CreditHistory, CreditType
 from app.exceptions import NotFoundError
+from app.logging import log_operation
 from app.responses import responses
 
 router = APIRouter(prefix="/credits", responses=responses)
@@ -14,6 +15,14 @@ async def add_credit(
     credit: CreditBase, session: SessionDep, current_tenant: CurrentTenant
 ) -> CreditHistory:
 
+    log_operation(
+        "CREATE",
+        "Credit",
+        current_tenant.id,
+        "PENDING",
+        detail=credit.model_dump(),
+    )
+
     account = session.exec(
         select(Account).where(
             Account.id == credit.account_id, Account.tenant_id == current_tenant.id
@@ -21,6 +30,16 @@ async def add_credit(
     ).first()
 
     if not account:
+
+        log_operation(
+            operation="CREATE",
+            model="Credit",
+            tenant_id=current_tenant.id,
+            status="FAILED",
+            detail=f"account id {credit.account_id} not found",
+            level="warning",
+        )
+
         raise NotFoundError(detail="Account not found")
 
     credit_history_db = CreditHistory.model_validate(
@@ -32,6 +51,13 @@ async def add_credit(
     session.commit()
     session.refresh(credit_history_db)
 
+    log_operation(
+        operation="CREATE",
+        model="Credit",
+        tenant_id=current_tenant.id,
+        status="SUCCESS",
+    )
+
     return credit_history_db
 
 
@@ -40,12 +66,31 @@ async def delete_credit(
     credit: CreditBase, session: SessionDep, current_tenant: CurrentTenant
 ) -> CreditHistory:
 
+    log_operation(
+        "DELETE",
+        "Credit",
+        current_tenant.id,
+        "PENDING",
+        detail=credit.model_dump(),
+    )
+
     account = session.exec(
         select(Account).where(
             Account.id == credit.account_id, Account.tenant_id == current_tenant.id
         )
     ).first()
+
     if not account:
+
+        log_operation(
+            operation="DELETE",
+            model="Credit",
+            tenant_id=current_tenant.id,
+            status="FAILED",
+            detail=f"account id {credit.account_id} not found",
+            level="warning",
+        )
+
         raise NotFoundError(detail="Account not found")
 
     credit_history_db = CreditHistory.model_validate(
@@ -57,5 +102,12 @@ async def delete_credit(
     session.add(credit_history_db)
     session.commit()
     session.refresh(credit_history_db)
+
+    log_operation(
+        operation="DELETE",
+        model="Credit",
+        tenant_id=current_tenant.id,
+        status="SUCCESS",
+    )
 
     return credit_history_db
