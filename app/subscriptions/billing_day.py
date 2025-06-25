17e5b8b3db02b3956e278ff6da04bb54f31cdce0
raulodev <raulodev@gmail.com>
2025-06-25 09:19:19 -0400

    add calculate bd

---
+++ app/subscriptions/billing_day.py
@@ -0,0 +1,24 @@
+from app.database.models import BillingPeriod
+
+
+def billing_day(biiling_period: BillingPeriod, day: int):
+    """
+    Billing day only applies to subscriptions
+    whose recurring term is month based -- e.g MONTHLY, ANNUAL, ..
+
+    Args:
+        biiling_period (BillingPeriod):
+        day (int):
+    """
+
+    if biiling_period in [
+        BillingPeriod.DAILY,
+        BillingPeriod.WEEKLY,
+        BillingPeriod.BIWEEKLY,
+        BillingPeriod.THIRTY_DAYS,
+        BillingPeriod.THIRTY_ONE_DAYS,
+    ]:
+
+        return None
+
+    return day
