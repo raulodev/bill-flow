1a6d5e179c3697b6abfe906a339f10172b3929e8
raulodev <raulodev@gmail.com>
2025-07-27 17:01:12 -0400

    refactor Clock class to enhance now() method with full datetime option

--- app/clock.py
+++ app/clock.py
@@ -4,9 +4,21 @@ from datetime import datetime, timezone
 class Clock:
     """Clock class."""
 
-    def now(self) -> datetime:
-        """Return the current datetime."""
-        return datetime.now(timezone.utc).replace(microsecond=0)
+    def now(self, full: bool = False) -> datetime:
+        """Get current time
+
+        Args:
+            full (bool, optional): Get full datetime. Defaults to False.
+
+        Returns:
+            datetime:
+        """
+
+        return (
+            datetime.now(timezone.utc)
+            if full
+            else datetime.now(timezone.utc).replace(microsecond=0)
+        )
 
 
 clock = Clock()
